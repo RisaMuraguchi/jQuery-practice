0d3cc11123bf93506ac5f65773555ce819c52401
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-06-15 21:59:12 +0900

    [Add]6-2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-$(document).ready(function () {
-  $("body").html("<h1>Hello jQuery!!</h1>");
-});
+// $(document).ready(function () {
+//   $("body").html("<h1>Hello jQuery!!</h1>");
+// });
+
+$("p").css("color","red");
