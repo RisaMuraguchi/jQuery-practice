275e2165540275a40f129e7ae3d0e71598cdf127
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-06-16 21:17:26 +0900

    [Add]6-2

--- app.js
+++ app.js
@@ -9,9 +9,15 @@
 // h1要素で、id属性が”h2ではない”要素の文字色を赤くする
 // $("h1[id!='h2']").css("color","red");
 
-$(function () {
-  $('.box1').on({
-    'click': function(){$('.box1').addClass('box1-ext');},
-    'mouseout': function(){$('.box1').removeClass('box1-ext');}
+// $(function () {
+  // $('.box1').on({
+    // 'click': function(){$('.box1').addClass('box1-ext');},
+    // 'mouseout': function(){$('.box1').removeClass('box1-ext');}
+  // });
+// });
+
+$(function() {
+  $(":button").click(function() {
+    $("#jqs-1 p span").text($(":text").val());
   });
 });
