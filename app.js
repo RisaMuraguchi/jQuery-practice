41fe2a8bf49e0ca53249cac290169b16017c6892
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-06-16 21:03:38 +0900

    [Add]6-2

--- app.js
+++ app.js
@@ -8,3 +8,10 @@
 // 属性セレクタ
 // h1要素で、id属性が”h2ではない”要素の文字色を赤くする
 // $("h1[id!='h2']").css("color","red");
+
+$(function () {
+  $('.box1').on({
+    'click': function(){$('.box1').addClass('box1-ext');},
+    'mouseout': function(){$('.box1').removeClass('box1-ext');}
+  });
+});
